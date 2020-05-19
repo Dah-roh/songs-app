be4120f5635de112be3e178b19ba7499c90fa328
dizsart <darogadibia@gmail.com>
2020-05-19 16:17:25 +0100

    import connect and exported it with songlist callback function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
     render( ) {
@@ -10,4 +11,4 @@ class SongList extends Component {
     }
 }
 
-export default SongList
+export default connect () (SongList);
