3104944417a0c53a21a242b7d0d38d4a86d7d33f
dizsart <darogadibia@gmail.com>
2020-05-19 21:31:41 +0100

    make list render song title

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,13 +2,30 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
-    render( ) {
-        return (
-            <div>
-                Song Lists
-            </div>
-        )
+    renderList( ) {
+        return this.props.songs.map((song)=> {
+            return(
+                <div className = "item" key={song.title}>
+                    <div className="right floated content"> 
+                    <button className="ui button primary">
+                        Select
+                        </button>
+                        </div>
+                        <div className="content">
+                            {song.title}
+                        </div>
+                </div>
+            )
+        })
+    }
+    render(){
+        return <div className="ui divided list">{this.renderList()}</div>
     }
 }
 
-export default connect () (SongList);
+const mapStateToProps = (state) => {
+
+    return {songs: state.songs};
+}
+
+export default connect (mapStateToProps) (SongList);
